f394f827d784eee2b45b04781a42f0a1c2afa2d2
mille <millenes137@gmail.com>
2026-08-12 11:59:05 -0300

    Remove dependências Firebase não utilizadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,9 +61,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
 
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
