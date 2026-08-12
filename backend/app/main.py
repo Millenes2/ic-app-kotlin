d6ac271a9ef54a295e66a04ec8dea8445347e8b9
mille <millenes137@gmail.com>
2026-08-12 12:03:41 -0300

    Restringe CORS para origens conhecidas

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,7 +62,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://10.0.2.2:8000", "http://localhost:8000"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
